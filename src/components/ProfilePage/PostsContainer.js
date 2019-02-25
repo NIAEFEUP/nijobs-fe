bc57c57f01dd4b413a1be9b45c65854e5e3c9a83
Duarte Oliveira <duartefmoliveira8@gmail.com>
2019-02-25 19:13:18 +0000

    Added posts placeholders to posts tabs

--- src/components/ProfilePage/PostsContainer.js
+++ src/components/ProfilePage/PostsContainer.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import style from './PostsContainer.module.css';
 
+
+const styles = {
+    post: {
+        height: "100px",
+        backgroundColor: "rgb(220, 79, 71)",
+        marginTop: "20px",
+        textAlign: "center",
+        borderRadius: "10px"
+    }
+};
 
 class PostsContainer extends Component {
     render() {
+        let rows = [];
+        for (let i = 1; i <= 10; i++) {
+            rows.push(
+                <Grid
+                    item
+                    xs={8}
+                    style={styles.post}
+                >
+                    Post
+                    {' ' + i}
+                </Grid>
+            );
+        }
+
         return (
-            <div>
-                Posts
-            </div>
+            <Grid
+                container
+                justify="center"
+                className={style.postsContainer}
+            >
+                {rows}
+            </Grid>
         );
     }
 }
