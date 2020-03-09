ec828f9b074bd39f507842f61c5f79e8df7be22e
Angelo Teixeira <imnotteixeira@gmail.com>
2020-03-09 00:51:45 +0000

    PoC with custom useSession

---
+++ src/hooks/useSession.js
@@ -0,0 +1,33 @@
+import useSWR from "swr";
+
+const API_HOSTNAME = "http://localhost:8087";
+
+export default () => {
+
+    const getSession = async (key) => {
+        const res = await fetch(key, {
+            method: "GET",
+            credentials: "include",
+        });
+
+        if (!res.ok) throw { status: res.status };
+        else return (await res.json()).data;
+    };
+
+    const { data, error, mutate } = useSWR(`${API_HOSTNAME}/auth/me`, getSession, {
+        revalidateOnFocus: false,
+        onErrorRetry: (error, key, option, revalidate, { retryCount }) => {
+            if (error.status === 401) return;
+
+            // retry after 5 seconds
+            setTimeout(() => revalidate({ retryCount: retryCount + 1 }), 5000);
+        },
+    });
+
+    return {
+        data,
+        error,
+        update: () => mutate({}),
+    };
+
+};
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,6 +4,8 @@ import MainView from "../components/HomePage/MainView";
 import SearchResultsWidget from "../components/HomePage/SearchResultsArea/SearchResultsWidget/SearchResultsWidget";
 import ProductDescription from "../components/HomePage/ProductPlacementArea/ProductDescription";
 import { smoothScrollToRef } from "../utils";
+import useSession from "../hooks/useSession";
+import { Button } from "@material-ui/core";
 
 export const HomePage = () => {
 
@@ -16,9 +18,39 @@ export const HomePage = () => {
         if (showSearchResults && searchResultsRef) smoothScrollToRef(searchResultsRef);
     }, [searchResultsRef, showSearchResults]);
 
+    const { data, update } = useSession();
+
+    const login = (e) => {
+        e.preventDefault();
+        fetch("http://localhost:8087/auth/login", {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                "email": "angelo@niaefeup.com",
+                "password": "password123",
+            }),
+        })
+            .then(() => update());
+    };
+
+    const logout = (e) => {
+        e.preventDefault();
+        fetch("http://localhost:8087/auth/login", {
+            method: "DELETE",
+            credentials: "include",
+        })
+            .then(() => update());
+    };
 
     return (
         <React.Fragment>
+            <Button onClick={login}>LOGIN</Button>
+            <Button onClick={logout}>LOGOUT</Button>
+            <Button onClick={() => update()}>UPFKNDATE</Button>
+            {data && data.email && `Hello ${data.email}`}
             <MainView
                 scrollToProductDescription={smoothScrollToRef.bind(null, productDescriptionRef)}
                 showSearchResults={() => {
