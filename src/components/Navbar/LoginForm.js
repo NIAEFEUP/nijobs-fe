f834203bcbd0176f9fb8e2915909c7a615ffc9fd
Angelo Teixeira <imnotteixeira@gmail.com>
2020-03-18 12:59:37 +0000

    Added login form validation with 'react-hook-form' and 'yup'

--- src/components/Navbar/LoginForm.js
+++ src/components/Navbar/LoginForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
     CircularProgress,
@@ -14,20 +14,44 @@ import {
 
 import { login } from "../../services/auth";
 import { useLoginStyles } from "./index";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
 
+const ASD_SCH = yup.object().shape({
+    email: yup.string()
+        .required("required, but change me")
+        .email("email, but change me"),
+    password: yup.string()
+        .required("required, but change me")
+        .min(8),
+});
 
 const LoginForm = ({ open, toggleLoginModal, loginPending, toggleLoginPending, updateSessionInfo }) => {
 
     const loginClasses = useLoginStyles();
 
-    const [email, setEmail] = React.useState("");
-    const [password, setPassword] = React.useState("");
-    const [loginError, setLoginError] = React.useState(null);
+    const { register, handleSubmit, reset, errors } = useForm({
+        mode: "onBlur",
+        validationSchema: ASD_SCH,
+        reValidateMode: "onChange",
+    });
 
-    const handleLogin = (e) => {
-        e.preventDefault();
+    const [errorCleared, setErrorCleared] = useState(true);
+
+
+    // const [email, setEmail] = React.useState("");
+    // const [password, setPassword] = React.useState("");
+    const [loginError, setLoginError] = React.useState(null);
+    const resetError = () => {
+        if (!errorCleared) {
+            setLoginError([]);
+            setErrorCleared(true);
+        }
+    };
+
+    const handleLogin = (data) => {
         toggleLoginPending();
-        login(email, password)
+        login(data.email, data.password)
             .then(() => {
                 updateSessionInfo();
                 toggleLoginPending();
@@ -41,67 +65,86 @@ const LoginForm = ({ open, toggleLoginModal, loginPending, toggleLoginPending, u
 
     const handleClose = () => {
         console.log("SHOULD RESET FORM HERE, INCLUDING ERRORS!!");
+        resetError();
+        reset();
         toggleLoginModal();
+    };
+
+    const onSubmit = (data) => {
+        setErrorCleared(false);
+        handleLogin(data);
     };
 
     return (
         <Dialog open={open} aria-labelledby="form-dialog-title" disableScrollLock onClose={handleClose}>
-            <DialogTitle id="form-dialog-title">Login</DialogTitle>
-            <DialogContent>
-                <DialogContentText>
+            <form
+                onSubmit={handleSubmit(onSubmit)}
+            >
+                <DialogTitle id="form-dialog-title">Login</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
                     Need to use redux-form-hook here in order to handle validation, reset and error handling more easily
-                </DialogContentText>
-                <TextField
-                    autoFocus
-                    id="email"
-                    label="Email"
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    margin="normal"
-                    fullWidth
-                />
-                <TextField
-                    id="password"
-                    label="Password"
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    margin="normal"
-                    fullWidth
-                />
-                <FormHelperText error={!!loginError}>
-                    {loginError || " "}
-                </FormHelperText>
-            </DialogContent>
-            <DialogActions>
-                <Button
-                    onClick={toggleLoginModal}
-                    variant="text"
-                    color="secondary"
-                    disabled={loginPending}
-                >
-                    Cancel
-                </Button>
-                <div className={loginClasses.loginBtnWrapper}>
+                    </DialogContentText>
+
+                    <TextField
+                        autoFocus
+                        id="email"
+                        name="email"
+                        label="Email"
+                        type="email"
+                        onChange={resetError}
+                        margin="normal"
+                        fullWidth
+                        inputRef={register}
+                        error={!!errors.email}
+                        helperText={errors.email ? errors.email.message : <span/>}
+                    />
+                    <TextField
+                        id="password"
+                        name="password"
+                        label="Password"
+                        type="password"
+                        onChange={resetError}
+                        margin="normal"
+                        fullWidth
+                        inputRef={register}
+                        error={!!errors.password}
+                        helperText={errors.password ? errors.password.message : <span/>}
+                    />
+                    <FormHelperText error={!!loginError}>
+                        {loginError || " "}
+                    </FormHelperText>
+                </DialogContent>
+                <DialogActions>
                     <Button
-                        className={loginClasses.loginBtn}
-                        onClick={(e) => {
-                            handleLogin(e, email, password);
-                        }}
-                        color="primary"
-                        variant="contained"
+                        onClick={toggleLoginModal}
+                        variant="text"
+                        color="secondary"
                         disabled={loginPending}
                     >
-                            Login
+                    Cancel
                     </Button>
-                    {loginPending &&
-                    <CircularProgress
-                        size={24}
-                        className={loginClasses.loginProgressRed}
-                    />}
-                </div>
-            </DialogActions>
+                    <div className={loginClasses.loginBtnWrapper}>
+                        <Button
+                            type="submit"
+                            className={loginClasses.loginBtn}
+                            // onClick={(e) => {
+                            //     handleLogin(e, email, password);
+                            // }}
+                            color="primary"
+                            variant="contained"
+                            disabled={loginPending}
+                        >
+                            Login
+                        </Button>
+                        {loginPending &&
+                        <CircularProgress
+                            size={24}
+                            className={loginClasses.loginProgressRed}
+                        />}
+                    </div>
+                </DialogActions>
+            </form>
         </Dialog>
     );
 };
