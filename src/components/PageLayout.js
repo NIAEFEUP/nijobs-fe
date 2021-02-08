8a7b8b895fb33cc13e6c57147285cd378ae32865
monkin <monkinsmurf@gmail.com>
2021-02-08 21:55:11 +0000

    Changed to functional component

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -1,18 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import ContactSection from "./HomePage/ContactSection";
 import propTypes from "prop-types";
 
-class PageLayout extends Component {
-
-    render() {
-        return (
-            <div>
-                {this.props.children}
-                <ContactSection />
-            </div>
-        );
-    }
-}
+const PageLayout = (props) => (
+    <div>
+        {props.children}
+        <ContactSection />
+    </div>
+);
 
 PageLayout.propTypes = {
     children: propTypes.isRequired,
