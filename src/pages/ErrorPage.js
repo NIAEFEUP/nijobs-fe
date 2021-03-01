4dc4333aedf7da36a0ac0e5288964083d73ee02d
Tiago Gomes <up201806658@fe.up.pt>
2021-03-01 21:29:07 +0000

    Removed emoji

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -3,7 +3,7 @@ import React from "react";
 import ErrorComponent from "../components/Errors/ErrorComponent";
 
 const ErrorPage = () => (
-    <ErrorComponent Message={"Something is wrong in the backstage. 😢"}/>
+    <ErrorComponent Message={"Something is wrong in the backstage. :("}/>
 );
 
 export default ErrorPage;
