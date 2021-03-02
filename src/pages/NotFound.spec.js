ad80b1802f75ec71b4decf00c8f9c1fddcd024fb
Tiago Gomes <up201806658@fe.up.pt>
2021-03-02 10:14:57 +0000

    Added test to not found page

--- src/pages/NotFound.spec.js
+++ src/pages/NotFound.spec.js
@@ -1,8 +1,17 @@
 import React from "react";
-import NotFound from "./NotFound";
+import { createMuiTheme } from "@material-ui/core";
+
+import ErrorComponent from "../components/Errors/ErrorComponent";
+import { mountWithTheme } from "../test-utils";
+
+const theme = createMuiTheme({});
 
 describe("NotFound page", () => {
     it("should render successfully", () => {
-        mount(<NotFound />);
+        const notFoundPage = mountWithTheme(
+            <ErrorComponent Message={"Sorry, no page could be found at this address (404)"}/>,
+            theme
+        ).find(ErrorComponent).first();
+        expect(notFoundPage.exists()).toBe(true);
     });
 });
