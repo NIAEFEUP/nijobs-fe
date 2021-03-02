bb21356d9d0a9d1d71d9135c83d8e6c37e9e92d9
Tiago Gomes <up201806658@fe.up.pt>
2021-03-02 11:07:41 +0000

    Changed mountWithTheme to renderWithTheme

--- src/pages/NotFound.spec.js
+++ src/pages/NotFound.spec.js
@@ -2,16 +2,17 @@ import React from "react";
 import { createMuiTheme } from "@material-ui/core";
 
 import ErrorComponent from "../components/Errors/ErrorComponent";
-import { mountWithTheme } from "../test-utils";
+import { renderWithTheme } from "../test-utils";
 
 const theme = createMuiTheme({});
 
 describe("NotFound page", () => {
     it("should render successfully", () => {
-        const notFoundPage = mountWithTheme(
-            <ErrorComponent Message={"Sorry, no page could be found at this address (404)"}/>,
-            theme
-        ).find(ErrorComponent).first();
-        expect(notFoundPage.exists()).toBe(true);
+        const errorMessage = "Sorry, no page could be found at this address (404)";
+        const wrapper = renderWithTheme(
+            <ErrorComponent Message={errorMessage}/>,
+            { theme }
+        );
+        expect(wrapper.queryByText(errorMessage)).toBeInTheDocument();
     });
 });
