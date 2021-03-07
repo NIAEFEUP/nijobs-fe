1d898a489789e60f960fab315e9e8676757ee2eb
Tiago Gomes <up201806658@fe.up.pt>
2021-03-07 16:36:15 +0000

    Fixed page not found test

--- src/pages/NotFound.spec.js
+++ src/pages/NotFound.spec.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { createMuiTheme } from "@material-ui/core";
 
-import ErrorComponent from "../components/Errors/ErrorComponent";
 import { renderWithTheme } from "../test-utils";
+import NotFound from "./NotFound";
 
 const theme = createMuiTheme({});
 
 describe("NotFound page", () => {
     it("should render successfully", () => {
-        const errorMessage = "Sorry, no page could be found at this address (404)";
         const wrapper = renderWithTheme(
-            <ErrorComponent message={errorMessage} />,
+            <NotFound />,
             { theme }
         );
-        expect(wrapper.queryByText(errorMessage)).toBeInTheDocument();
+        expect(wrapper.queryByText("Page not found")).toBeInTheDocument();
+        expect(wrapper.queryByText("Sorry, no page could be found at this address (404)")).toBeInTheDocument();
     });
 });
