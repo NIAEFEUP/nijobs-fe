55b7b94dc112a5480b439647b96be015f2b08371
monkin <monkinsmurf@gmail.com>
2021-03-21 13:39:55 +0000

    Changed logic to who can see the myOffersPage on the navbar + fixed styling issues

--- src/components/Navbar/UserMenu.js
+++ src/components/Navbar/UserMenu.js
@@ -100,8 +100,10 @@ const UserMenuContent = React.forwardRef(({ open, isMobile = false, sessionData,
                 autoFocusItem={open}
                 id="menu-list-grow"
             >
-                <MenuItem button component={Link} to="/company/offers/manage" disableTouchRipple>My Offers</MenuItem>
-                <MenuItem button disableTouchRipple onClick={() => {}}>Profile</MenuItem>
+                {!!sessionData?.company &&
+                <MenuItem button component={Link} to="/company/offers/manage" disableTouchRipple>My Offers</MenuItem>}
+
+                {/* <MenuItem button disableTouchRipple onClick={() => {}}>Profile</MenuItem> */}
                 <MenuItem button disableTouchRipple onClick={handleLogout}>Logout</MenuItem>
                 {sessionData?.isAdmin && <AdminMenuOptions isMobile={isMobile} />}
             </MenuList>
@@ -115,6 +117,7 @@ UserMenuContent.propTypes = {
     sessionData: PropTypes.shape({
         email: PropTypes.string,
         isAdmin: PropTypes.bool,
+        company: PropTypes.object,
     }),
     handleLogout: PropTypes.func.isRequired,
 };
--- src/pages/MyOffersPage.js
+++ src/pages/MyOffersPage.js
@@ -1,17 +1,29 @@
 import React from "react";
-import { Paper } from "@material-ui/core";
+import { Card, makeStyles } from "@material-ui/core";
 import CenteredComponent from "../components/HomePage/CenteredComponent";
 import { MainMask } from "../components/HomePage/MainMask";
 
-export const MyOffersPage = () => (
-    <>
-        <MainMask />
-        <CenteredComponent>
-            <Paper style={{ width: "60%", padding: "24px 72px", boxSizing: "content-box" }}>
-                <h1>My Offers Page</h1>
-            </Paper>
-        </CenteredComponent>
-    </>
-);
+const useStyles = makeStyles(() => ({
+    cardContainer: {
+        width: "60%",
+        padding: "24px 72px",
+        boxSizing: "content-box",
+    },
+}));
+
+export const MyOffersPage = () => {
+    const classes = useStyles();
+    return (
+        <>
+            <MainMask />
+            <CenteredComponent>
+                <Card className={classes.cardContainer}>
+                    <h1>My Offers Page</h1>
+                </Card>
+            </CenteredComponent>
+        </>
+    );
+};
+
 
 export default MyOffersPage;
