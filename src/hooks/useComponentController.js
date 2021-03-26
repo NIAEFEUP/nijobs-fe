750533562fd4be362d751cbcdf2c0033ecbb93d2
Angelo Teixeira <imnotteixeira@gmail.com>
2021-03-26 19:08:35 +0000

    Added reactive controller pattern to PageLayout

--- src/hooks/useComponentController.js
+++ src/hooks/useComponentController.js
@@ -1,11 +1,16 @@
-export default (controller, controllerParams, Context) => {
+import React from "react";
+
+const DefaultContext = React.createContext();
+
+export default (controller = () => ({}), controllerParams, Context) => {
 
     const { controllerOptions, ...controllerOutput } = controller(controllerParams);
 
-
+    const ContextProvider = Context ? Context.Provider : DefaultContext.Provider;
+    const contextProviderProps = Context ? { value: controllerOptions?.initialValue || {} } : {};
     return {
         ...controllerOutput,
-        ContextProvider: Context.Provider,
-        contextProviderProps: { value: controllerOptions?.initialValue || {} },
+        ContextProvider,
+        contextProviderProps,
     };
 };
