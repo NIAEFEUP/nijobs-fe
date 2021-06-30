ce91fe74beb44f19acc64b2eb5015e77bbc2f1a0
monkin <monkinsmurf@gmail.com>
2021-06-30 18:56:29 +0100

    Added render test for CompanyOffersManagementWidget

---
+++ src/components/Company/Offers/Manage/CompanyOffersManagementWidget.spec.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import CompanyOffersManagementWidget from "./CompanyOffersManagementWidget";
+import { renderWithStore } from "../../../../test-utils";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
+
+
+describe("App", () => {
+    test("Renders Component", async () => {
+        renderWithStore(
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <CompanyOffersManagementWidget />
+            </MuiPickersUtilsProvider>
+        );
+
+        // Use getBy by default when element should be available
+        expect(screen.getByText("Loading...")).toBeInTheDocument();
+
+        expect(screen.getByRole("heading")).toBeInTheDocument();
+
+        // Use queryBy When asserting for missing element
+        expect(screen.queryByText("Offers Management")).toBeNull();
+
+        screen.debug();
+
+        // Use find by for elements that aren't there initially, but will be eventually
+        expect(await screen.findByText("Offers Management")).toBeInTheDocument();
+
+        screen.debug();
+    });
+});
