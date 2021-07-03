5f2999b191956dd84747a1a2575e3e07f31d9921
monkin <monkinsmurf@gmail.com>
2021-07-03 20:50:46 +0100

    Fixed bug where it only returned 1 offer and removed mock offers

--- src/services/companyOffersService.js
+++ src/services/companyOffersService.js
@@ -1,100 +1,14 @@
-import Offer from "../components/HomePage/SearchResultsArea/Offer/Offer";
-
-
 import config from "../config";
 const { API_HOSTNAME } = config;
-
-
-// TODO remove this
-const redditLogo = require("./reddit-logo.png");
-
-// eslint-disable-next-line no-unused-vars
-const MOCK_OFFERS = [
-    new Offer({
-        id: "random uuid1",
-        title: "Full-Stack Developer",
-        company: {
-            name: "Reddit",
-            logo: redditLogo,
-        },
-        location: "San Francisco",
-        date: "2019-06",
-        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-    }),
-    new Offer({
-        id: "random uuid2",
-        title: "Security Guy",
-        company: {
-            name: "Reddit",
-            logo: redditLogo,
-        },
-        location: "Porto",
-        date: "2019-06",
-        description: "You won't do much, really...",
-    }),
-    new Offer({
-        id: "random uuid3",
-        title: "Frontend Developer But Make it Longer",
-        company: {
-            name: "Reddit",
-            logo: redditLogo,
-        },
-        location: "Porto",
-        date: "2019-06",
-        description: "kek",
-    }),
-    new Offer({
-        id: "random uuid4",
-        title: "Guy in the background",
-        company: {
-            name: "Reddit",
-            logo: redditLogo,
-        },
-        location: "Porto",
-        date: "2019-06",
-        description: "kek",
-    }),
-    new Offer({
-        id: "random uuid5",
-        title: "Janitor",
-        company: {
-            name: "Reddit",
-            logo: redditLogo,
-        },
-        location: "Porto",
-        date: "2019-06",
-        description: "kek",
-    }),
-    new Offer({
-        id: "random uuid6",
-        title: "Fullstack developer but make it shorter",
-        company: {
-            name: "Reddit",
-            logo: redditLogo,
-        },
-        location: "Porto",
-        date: "2019-06",
-        description: "kek",
-    }),
-];
-
-// REMOVE THIS LATER; WHEN THE FETCH CALL IS UNCOMMENTED
-// eslint-disable-next-line no-unused-vars
 
 const parseFiltersToURL = (filters) => Object.keys(filters).map((key) => `${key}=${filters[key]}`).join("&");
 
 export const fetchCompanyOffers = async (companyID, filters) => {
-    await sleep(500);
+    await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("resolved");
+        }, 500);
+    });
 
     try {
         const res = await fetch(`${API_HOSTNAME}/offers/company/${companyID}${filters ? `?${parseFiltersToURL(filters)}` : ""}`, {
@@ -114,8 +28,4 @@ export const fetchCompanyOffers = async (companyID, filters) => {
         if (Array.isArray(error)) throw error;
         throw [{ msg: "Unexpected Error" }];
     }
-
-    // throw new Error("An error occurred!");
-
-    // return MOCK_OFFERS;
 };
