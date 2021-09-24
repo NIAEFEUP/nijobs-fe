ba66ca5352e3afffd9d06b9be8ff0ec5c51a110e
Tiago Gomes <up201806658@fe.up.pt>
2021-09-24 15:06:05 +0100

    Improved scroll behavior in the home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,18 @@
 import React, { useState, useRef, useEffect } from "react";
-
+import { makeStyles } from "@material-ui/core";
 import MainView from "../components/HomePage/MainView";
-import SearchResultsWidget from "../components/HomePage/SearchResultsArea/SearchResultsWidget/SearchResultsWidget";
-import ProductDescription from "../components/HomePage/ProductPlacementArea/ProductDescription";
 import ApplicationMessagesNotifier from "../components/ApplicationMessages";
 import { smoothScrollToRef } from "../utils";
 import { useDesktop } from "../utils/media-queries";
+import ProductDescription from "../components/HomePage/ProductPlacementArea/ProductDescription";
+import SearchResultsWidget from "../components/HomePage/SearchResultsArea/SearchResultsWidget/SearchResultsWidget";
+
+const useStyles = makeStyles(() => ({
+    searchResults: {
+        scrollSnapAlign: "start",
+        scrollMarginBottom: "100vh", // don't apply snap scroll to the bottom of the search results, so that it's possible to see the footer
+    },
+}));
 
 export const HomePage = () => {
 
@@ -17,7 +24,12 @@ export const HomePage = () => {
     // this will not trigger the scroll on subsequent submits, because the dependencies won't change after the first call
     useEffect(() => {
         if (showSearchResults && searchResultsRef) smoothScrollToRef(searchResultsRef);
+
+        // It's mandatory to add the scroll-snap-type style property to the html tag in order to use snap-scroll
+        document.getElementsByTagName("html")[0].style.scrollSnapType = "y proximity";
     }, [searchResultsRef, showSearchResults]);
+
+    const classes = useStyles();
 
     return (
         <React.Fragment>
@@ -29,8 +41,13 @@ export const HomePage = () => {
                     if (searchResultsRef && searchResultsRef.current) smoothScrollToRef(searchResultsRef);
                 }}
             />
-            <ProductDescription ref={productDescriptionRef} />
-            {showSearchResults && <SearchResultsWidget ref={searchResultsRef} />}
+            <ProductDescription ref={productDescriptionRef}  />
+
+            {showSearchResults &&
+                <div className={classes.searchResults}>
+                    <SearchResultsWidget ref={searchResultsRef} />
+                </div>
+            }
         </React.Fragment>
     );
 
