ec5a7416ab763556f023c857c90dc3d6b669ad8e
monkin <monkinsmurf@gmail.com>
2021-10-16 13:26:49 +0100

    Made fetchCompanyOffers a cancellable request

--- src/services/companyOffersService.js
+++ src/services/companyOffersService.js
@@ -1,42 +1,45 @@
 import config from "../config";
-import { parseFiltersToURL } from "../utils";
 import { createEvent, EVENT_TYPES, TIMED_ACTIONS, measureTime } from "../utils/analytics";
 import ErrorTypes from "../utils/ErrorTypes";
+import { buildCancelableRequest, parseFiltersToURL } from "../utils";
+
 const { API_HOSTNAME } = config;
 
 const COMPANY_OFFERS_FETCH_METRIC_ID = "company_offers/fetch";
 
-export const fetchCompanyOffers = measureTime(TIMED_ACTIONS.COMPANY_OFFERS_FETCH, async (companyID, filters) => {
-    let isErrorRegistered = false;
-    try {
-        const res = await fetch(`${API_HOSTNAME}/offers/company/${companyID}${filters ? `?${parseFiltersToURL(filters)}` : ""}`, {
-            method: "GET",
-            credentials: "include",
-        });
-        const json = await res.json();
+export const fetchCompanyOffers = measureTime(TIMED_ACTIONS.COMPANY_OFFERS_FETCH,
+    buildCancelableRequest(async (companyID, filters, { signal } = {}) => {
+        let isErrorRegistered = false;
+        try {
+            const res = await fetch(`${API_HOSTNAME}/offers/company/${companyID}${filters ? `?${parseFiltersToURL(filters)}` : ""}`, {
+                method: "GET",
+                credentials: "include",
+                signal,
+            });
+            const json = await res.json();
 
-        if (!res.ok) {
+            if (!res.ok) {
 
-            createEvent(EVENT_TYPES.ERROR(
+                createEvent(EVENT_TYPES.ERROR(
+                    COMPANY_OFFERS_FETCH_METRIC_ID,
+                    ErrorTypes.BAD_RESPONSE,
+                    res.status
+                ));
+                isErrorRegistered = true;
+
+                throw json.errors;
+            }
+
+            createEvent(EVENT_TYPES.SUCCESS(COMPANY_OFFERS_FETCH_METRIC_ID));
+            return json;
+        } catch (error) {
+
+            if (!isErrorRegistered) createEvent(EVENT_TYPES.ERROR(
                 COMPANY_OFFERS_FETCH_METRIC_ID,
-                ErrorTypes.BAD_RESPONSE,
-                res.status
+                ErrorTypes.NETWORK_FAILURE
             ));
-            isErrorRegistered = true;
 
-            throw json.errors;
+            if (Array.isArray(error)) throw error;
+            throw [{ msg: "Unexpected Error" }];
         }
-
-        createEvent(EVENT_TYPES.SUCCESS(COMPANY_OFFERS_FETCH_METRIC_ID));
-        return json;
-    } catch (error) {
-
-        if (!isErrorRegistered) createEvent(EVENT_TYPES.ERROR(
-            COMPANY_OFFERS_FETCH_METRIC_ID,
-            ErrorTypes.NETWORK_FAILURE
-        ));
-
-        if (Array.isArray(error)) throw error;
-        throw [{ msg: "Unexpected Error" }];
-    }
-});
+    }));
