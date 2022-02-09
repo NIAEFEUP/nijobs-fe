e5cb16b5096a78d6cd6eedf3fe1304d827fb3956
monkin77 <monkinsmurf@gmail.com>
2022-02-09 12:02:44 +0000

    Made Page adaptive to mobile to change padding

--- src/pages/ApplicationsReviewPage.js
+++ src/pages/ApplicationsReviewPage.js
@@ -1,16 +1,18 @@
 import { CardContent, makeStyles } from "@material-ui/core";
 import React from "react";
 import ApplicationsReviewWidget from "../components/Review/Applications/ApplicationsReviewWidget";
+import { useMobile } from "../utils/media-queries";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = (isMobile) => makeStyles((theme) => ({
     content: {
-        padding: theme.spacing(3, 9),
+        padding: isMobile ? theme.spacing(2, 4) : theme.spacing(3, 9),
         boxSizing: "content-box",
     },
 }));
 
 const ApplicationsReviewPage = () => {
-    const classes = useStyles();
+    const classes = useStyles(useMobile())();
+
     return (
         <CardContent className={classes.content}>
             <ApplicationsReviewWidget />
