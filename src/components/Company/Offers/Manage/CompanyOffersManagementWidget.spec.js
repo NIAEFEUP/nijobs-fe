9104d12dde5425f31470ae34806574faae516954
monkin77 <monkinsmurf@gmail.com>
2022-02-26 12:35:30 +0000

    Added mobile columns test to  CompanyOffersManagementWIdget

--- src/components/Company/Offers/Manage/CompanyOffersManagementWidget.spec.js
+++ src/components/Company/Offers/Manage/CompanyOffersManagementWidget.spec.js
@@ -2,19 +2,26 @@ import React from "react";
 import { screen, waitFor } from "@testing-library/react";
 
 import CompanyOffersManagementWidget from "./CompanyOffersManagementWidget";
-import { renderWithStore } from "../../../../test-utils";
+import { renderWithStoreAndTheme } from "../../../../test-utils";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import DateFnsUtils from "@date-io/date-fns";
 import * as companyOffersService from "../../../../services/companyOffersService";
 import useSession from "../../../../hooks/useSession";
 import { BrowserRouter } from "react-router-dom";
 import { addSnackbar } from "../../../../actions/notificationActions";
+import { createMatchMedia } from "../../../../utils/media-queries";
+import { columns } from "./CompanyOffersManagementSchema";
+import { createTheme } from "@material-ui/core";
+import { SnackbarProvider } from "notistack";
+import Notifier from "../../../Notifications/Notifier";
 
 jest.mock("../../../../hooks/useSession");
 jest.mock("../../../../services/companyOffersService");
 jest.mock("../../../../actions/notificationActions");
 
 describe("App", () => {
+    const theme = createTheme({});
+
     const MOCK_OFFERS = [
         {
             _id: "random uuid4",
@@ -44,13 +51,13 @@ describe("App", () => {
         useSession.mockReturnValue({ data: { company: { name: "company1", _id: "company_id" } }, isLoggedIn: true });
     });
 
-    test("Renders Loading", () => {
+    it("Renders Loading", () => {
         companyOffersService.fetchCompanyOffers.mockImplementationOnce(() => new Promise(() => {}));
 
-        renderWithStore(
+        renderWithStoreAndTheme(
             <MuiPickersUtilsProvider utils={DateFnsUtils}>
                 <CompanyOffersManagementWidget />
-            </MuiPickersUtilsProvider>
+            </MuiPickersUtilsProvider>, { initialState: {}, theme }
         );
 
         // Use getBy by default when element should be available
@@ -62,19 +69,19 @@ describe("App", () => {
         expect(screen.getByText("Offers Management")).toBeInTheDocument();
     });
 
-    test("Loads Valid Offers", async () => {
+    it("Loads Valid Offers", async () => {
         companyOffersService.fetchCompanyOffers.mockImplementationOnce(() =>  new Promise((resolve) =>
             resolve(MOCK_OFFERS)
         ));
 
         // By waiting for act it executes all the async code at once
         // Need to wrap with BrowserRouter since there is a Link inside
-        renderWithStore(
+        renderWithStoreAndTheme(
             <BrowserRouter>
                 <MuiPickersUtilsProvider utils={DateFnsUtils}>
                     <CompanyOffersManagementWidget />
                 </MuiPickersUtilsProvider>
-            </BrowserRouter>
+            </BrowserRouter>, { initialState: {}, theme }
         );
 
         // wait for the wrapped assertions to pass within a certain timeout window (wait for all updates to complete)
@@ -86,15 +93,15 @@ describe("App", () => {
         });
     });
 
-    test("Loads Empty Offers", async () => {
+    it("Loads Empty Offers", async () => {
         companyOffersService.fetchCompanyOffers.mockImplementationOnce(() =>  new Promise((resolve) =>
             resolve([])
         ));
         // By waiting for act it executes all the async code at once
-        renderWithStore(
+        renderWithStoreAndTheme(
             <MuiPickersUtilsProvider utils={DateFnsUtils}>
                 <CompanyOffersManagementWidget />
-            </MuiPickersUtilsProvider>
+            </MuiPickersUtilsProvider>, { initialState: {}, theme }
         );
 
         await waitFor(() => {
@@ -106,7 +113,7 @@ describe("App", () => {
         });
     });
 
-    test("Error fetching offers", async () => {
+    it("Error fetching offers", async () => {
         addSnackbar.mockImplementationOnce(() => ({ type: "" }));
 
         companyOffersService.fetchCompanyOffers.mockImplementationOnce(() => new Promise((resolve, reject) =>
@@ -114,10 +121,10 @@ describe("App", () => {
         ));
 
         // By waiting for act it executes all the async code at once
-        renderWithStore(
+        renderWithStoreAndTheme(
             <MuiPickersUtilsProvider utils={DateFnsUtils}>
                 <CompanyOffersManagementWidget />
-            </MuiPickersUtilsProvider>
+            </MuiPickersUtilsProvider>, { initialState: {}, theme }
         );
 
         await waitFor(() => {
@@ -126,6 +133,39 @@ describe("App", () => {
             expect(screen.getByText("No offers here.")).toBeInTheDocument();
 
             expect(addSnackbar).toHaveBeenCalledTimes(1);
+        }, {
+            timeout: 1000,
+        });
+    });
+
+    it("Should only render mobile columns on mobile device", async () => {
+        addSnackbar.mockImplementationOnce(() => ({ type: "" }));
+
+        const MOBILE_WIDTH_PX = 360;
+        window.matchMedia = createMatchMedia(MOBILE_WIDTH_PX);
+
+        companyOffersService.fetchCompanyOffers.mockImplementationOnce(() =>  new Promise((resolve) =>
+            resolve(MOCK_OFFERS)
+        ));
+
+        renderWithStoreAndTheme(
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <SnackbarProvider maxSnack={3}>
+                    <Notifier />
+                    <CompanyOffersManagementWidget isMobile={true} />
+                </SnackbarProvider>
+            </MuiPickersUtilsProvider>, { initialState: {}, theme }
+        );
+
+        const mobileCols = ["title", "publishStartDate", "actions"];
+        await waitFor(() => {
+            for (const [col, val] of Object.entries(columns)) {
+                if (mobileCols.includes(col)) {
+                    expect(screen.getByRole("button", { name: val.label })).toBeInTheDocument();
+                } else {
+                    expect(screen.queryByRole("button", { name: val.label })).not.toBeInTheDocument();
+                }
+            }
         }, {
             timeout: 1000,
         });
