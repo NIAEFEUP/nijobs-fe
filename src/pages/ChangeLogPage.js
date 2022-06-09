594783aee29097f66a7e3f99194c9c17b62470a6
monkin77 <monkinsmurf@gmail.com>
2022-06-09 22:57:43 +0100

    Completed Page header

--- src/pages/ChangeLogPage.js
+++ src/pages/ChangeLogPage.js
@@ -1,18 +1,31 @@
-import { makeStyles } from "@material-ui/core";
+import { makeStyles, Typography } from "@material-ui/core";
 import React from "react";
 import { useMobile } from "../utils/media-queries";
 
-const useStyles = (isMobile) => makeStyles((theme) => ({
-
+const useStyles = makeStyles((theme) => ({
+    content: ({ isMobile }) => ({
+        padding: isMobile ? theme.spacing(0, 1) : theme.spacing(5),
+    }),
+    gray: {
+        color: "rgba(0, 0, 0, 0.54)",
+    },
 }));
 
 export const ChangeLogPage = () => {
     const isMobile = useMobile();
-    const classes = useStyles(isMobile)();
+    const classes = useStyles({ isMobile: isMobile });
 
     return (
-        <div>
-            <h1>Change Log Page</h1>
+        <div className={classes.content}>
+            <Typography variant="h3" id="Change-Log" data-id="Change-Log" gutterBottom>
+                Change Log
+            </Typography>
+
+            <Typography className={classes.gray} paragraph={true}>
+                Last updated: March 1, 2022 {/* GET FROM THE MOST UPDATED RELEASE */}
+            </Typography>
+
+
         </div>
     );
 };
