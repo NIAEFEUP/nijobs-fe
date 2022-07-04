1eb556da57ef81eda0cb5df9f651c0ab0f8142f6
Monkin <monkinsmurf@gmail.com>
2022-07-04 16:58:36 +0100

    Implemented changeLogService and fetching the releases in the changeLogPage

--- src/pages/ChangeLogPage.js
+++ src/pages/ChangeLogPage.js
@@ -1,5 +1,8 @@
 import { makeStyles, Typography } from "@material-ui/core";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { addSnackbar } from "../actions/notificationActions";
+import { fetchReleases } from "../services/changeLogService";
 import { useMobile } from "../utils/media-queries";
 
 const useStyles = makeStyles((theme) => ({
@@ -11,23 +14,53 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-export const ChangeLogPage = () => {
+export const ChangeLogPage = ({ addSnackbar }) => {
     const isMobile = useMobile();
     const classes = useStyles({ isMobile: isMobile });
 
+    const [releases, setReleases] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const releases = await fetchReleases();
+            setReleases(releases);
+            console.log("Releases -> ", releases);
+        };
+
+        fetchData().catch((err) => {
+            console.error(err);
+            // setError(err[0]);
+            // setIsLoading(false);
+
+            addSnackbar({
+                message:
+                    "An unexpected error occurred, please try refreshing the browser window.",
+                key: `${Date.now()}-fetchReleasesError`,
+            });
+        });
+    }, []);
+
     return (
         <div className={classes.content}>
-            <Typography variant="h3" id="Change-Log" data-id="Change-Log" gutterBottom>
+            <Typography
+                variant="h3"
+                id="Change-Log"
+                data-id="Change-Log"
+                gutterBottom
+            >
                 Change Log
             </Typography>
 
             <Typography className={classes.gray} paragraph={true}>
-                Last updated: March 1, 2022 {/* GET FROM THE MOST UPDATED RELEASE */}
+                Last updated: March 1, 2022
+                {/* GET FROM THE MOST UPDATED RELEASE */}
             </Typography>
-
-
         </div>
     );
 };
 
-export default ChangeLogPage;
+const mapDispatchToProps = (dispatch) => ({
+    addSnackbar: (notification) => dispatch(addSnackbar(notification)),
+});
+
+export default connect(null, mapDispatchToProps)(ChangeLogPage);
