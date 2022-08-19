3fcf95ebd843f9160c549acc0a43fa5ca96cf408
André Moreira <andre.moreira.9@hotmail.com>
2022-08-19 13:07:23 +0200

    Fix tests regarding company applications

--- src/components/Apply/Company/CompanyApplicationUtils.spec.js
+++ src/components/Apply/Company/CompanyApplicationUtils.spec.js
@@ -1,9 +1,9 @@
+import Constants from "../../../utils/Constants";
 import { getHumanError } from "./CompanyApplicationUtils";
 describe("CompanyApplicationUtils", () => {
-
     it("should return a readable error", () => {
         expect(getHumanError("email-already-exists")).toBe("The provided email is already associated to our platform.");
         expect(getHumanError("company-application-duplicate-email")).toBe("There is already an application associated with that email.");
-        expect(getHumanError("random-error")).toBe("An error occurred, please try again.");
+        expect(getHumanError("random-error")).toBe(Constants.UNEXPECTED_ERROR_MESSAGE);
     });
 });
