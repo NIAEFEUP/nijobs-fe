425b8e085bc62445e3886caaa6a03767f50ce756
Nuno Pereira <nunoafonso2002@gmail.com>
2022-09-14 13:52:33 +0100

    Made it so auto-submit only happens if one of the valid search params is present

--- src/components/HomePage/URLSearchParamsParser.js
+++ src/components/HomePage/URLSearchParamsParser.js
@@ -2,6 +2,8 @@ import { useMemo, useEffect } from "react";
 
 import qs from "qs";
 import { useLocation } from "react-router";
+import { useSelector } from "react-redux";
+
 import useOffersSearcher from "./SearchResultsArea/SearchResultsWidget/useOffersSearcher";
 import { SearchResultsConstants } from "./SearchResultsArea/SearchResultsWidget/SearchResultsUtils";
 
@@ -9,9 +11,12 @@ const URLSearchParamsParser = ({ showSearchResults }) => {
 
     const location = useLocation();
 
+    const offerSearch = useSelector((state) => state.offerSearch);
+    const validQueryParams = Object.keys(offerSearch);
+
     const queryParams = useMemo(() => qs.parse(location.search, {
         ignoreQueryPrefix: true,
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }), []);
 
     const { search: searchOffers } = useOffersSearcher({
@@ -26,12 +31,13 @@ const URLSearchParamsParser = ({ showSearchResults }) => {
     // we specifically want this to only run once to avoid infinite re-renders
     useEffect(() => {
 
-        if (Object.keys(queryParams).length) {
+        // filter to make sure we only submit form data if valid search params are present
+        if (Object.keys(queryParams).filter((key) => validQueryParams.includes(key)).length) {
             searchOffers(SearchResultsConstants.INITIAL_LIMIT);
 
             showSearchResults();
         }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return null;
