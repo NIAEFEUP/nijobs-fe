5c55d08e3cbf34f5f73d64383d00cae450d3989e
Nuno Pereira <nunoafonso2002@gmail.com>
2022-10-11 19:26:13 +0100

    Added tests for new widget

---
+++ src/components/HomePage/SearchResultsArea/SearchResultsWidget/SearchURLWidget.spec.js
@@ -0,0 +1,84 @@
+import React from "react";
+
+import { renderWithStoreAndTheme, fireEvent, act, screen } from "../../../../test-utils";
+import AppTheme from "../../../../AppTheme";
+import { addSnackbar } from "../../../../actions/notificationActions";
+
+import SearchUrlWidget from "./SearchURLWidget";
+
+jest.mock("../../../../actions/notificationActions");
+
+const writeText = jest.fn();
+
+Object.defineProperty(navigator, "clipboard", {
+    value: {
+        writeText,
+    },
+});
+
+describe("Search URL Widget", () => {
+
+    describe("render", () => {
+        it("Should render the current URL", () => {
+
+            const testString = "test-url";
+
+            global.window = Object.create(window);
+            Object.defineProperty(window, "location", {
+                value: {
+                    href: testString,
+                },
+                writable: true,
+            });
+
+            renderWithStoreAndTheme(<SearchUrlWidget />, { initialState: {}, theme: AppTheme });
+
+            expect(screen.getByText(testString)).toBeInTheDocument();
+        });
+    });
+
+    describe("interaction", () => {
+
+        beforeEach(() => {
+            addSnackbar.mockClear();
+        });
+
+        it("Should copy URL to clipboard", () => {
+
+            addSnackbar.mockImplementationOnce(() => ({ type: "" }));
+
+            const testString = "test-string";
+
+            global.window = Object.create(window);
+            Object.defineProperty(window, "location", {
+                value: {
+                    href: testString,
+                },
+                writable: true,
+            });
+
+            renderWithStoreAndTheme(<SearchUrlWidget />, { initialState: {}, theme: AppTheme });
+
+            act(() => {
+                fireEvent.click(screen.getByRole("button", { name: "Copy" }));
+            });
+
+            expect(writeText).toHaveBeenCalledTimes(1);
+            expect(writeText).toHaveBeenCalledWith(testString);
+        });
+
+        it("Should create a snackbar once URL is copied", () => {
+
+            addSnackbar.mockImplementation(jest.requireActual("../../../../actions/notificationActions").addSnackbar);
+
+            renderWithStoreAndTheme(<SearchUrlWidget />, { initialState: {}, theme: AppTheme });
+
+            act(() => {
+                fireEvent.click(screen.getByRole("button", { name: "Copy" }));
+            });
+
+            expect(addSnackbar).toHaveBeenCalledTimes(1);
+        });
+    });
+
+});
