4f9f8eb8a87958a38fa2710d843c8566466a7b35
Nuno Pereira <nunoafonso2002@gmail.com>
2022-11-12 18:41:15 +0000

    Updated query filtering

--- src/components/HomePage/URLSearchParamsParser.js
+++ src/components/HomePage/URLSearchParamsParser.js
@@ -7,12 +7,19 @@ import { useSelector } from "react-redux";
 import useOffersSearcher from "./SearchResultsArea/SearchResultsWidget/useOffersSearcher";
 import { SearchResultsConstants } from "./SearchResultsArea/SearchResultsWidget/SearchResultsUtils";
 
+// offerSearch params that should NOT trigger an auto-submit
+const invalidParams = ["error", "offers", "loading", "filterJobDuration"];
+
 const URLSearchParamsParser = ({ showSearchResults }) => {
 
     const location = useLocation();
 
     const offerSearch = useSelector((state) => state.offerSearch);
-    const validQueryParams = Object.keys(offerSearch);
+
+    const validQueryParams = Object.keys(offerSearch).filter((field) => !(invalidParams.includes(field)));
+    if (validQueryParams.includes("jobDuration")) {
+        validQueryParams.splice(validQueryParams.indexOf("jobDuration"), 1, "jobMinDuration", "jobMaxDuration");
+    }
 
     const queryParams = useMemo(() => qs.parse(location.search, {
         ignoreQueryPrefix: true,
