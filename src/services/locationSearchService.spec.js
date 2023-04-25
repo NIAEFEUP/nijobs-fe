4bc76aeb54854c3b0f0aad002f18681fcb007ef7
Nuno Pereira <nunoafonso2002@gmail.com>
2023-04-25 16:41:45 +0100

    Added more service tests

--- src/services/locationSearchService.spec.js
+++ src/services/locationSearchService.spec.js
@@ -1,5 +1,7 @@
 
 import config from "../config";
+import Constants from "../utils/Constants";
+
 import { searchCities } from "./locationSearchService";
 
 const API_ENDPOINT = `${config.LOCATION_SERVICE_HOSTNAME}/search`;
@@ -14,7 +16,7 @@ describe("Location Service", () => {
         ([k, v]) => `${encodeURIComponent(k)}=${encodeURIComponent(v)}`
     ).join("&")}`;
 
-    it("Should send a GET request to search for cities", async () => {
+    it("Should send a GET request to search for cities with default limit", async () => {
 
         const cities = [
             {
@@ -30,14 +32,41 @@ describe("Location Service", () => {
 
         const searchTerm = "test";
         const data = {
+            limit: 10,
             searchTerm,
         };
 
         const res = await searchCities(searchTerm);
 
-        expect(fetch).toHaveBeenCalledWith(`${API_ENDPOINT}?${buildUriParams({ limit: 10, ...data  })}`);
+        expect(fetch).toHaveBeenCalledWith(`${API_ENDPOINT}?${buildUriParams(data)}`);
         expect(res).toEqual(cities);
+    });
 
+    it("Should send a GET request to search for cities with custom limit", async () => {
+
+        const cities = [
+            {
+                city: "test",
+                country: "test",
+                latitude: 0,
+                longitude: 0,
+            },
+        ];
+
+        // Simulate request success
+        fetch.mockResponse(JSON.stringify(cities));
+
+        const searchTerm = "test";
+        const limit = 5;
+        const data = {
+            limit,
+            searchTerm,
+        };
+
+        const res = await searchCities(searchTerm, limit);
+
+        expect(fetch).toHaveBeenCalledWith(`${API_ENDPOINT}?${buildUriParams(data)}`);
+        expect(res).toEqual(cities);
     });
 
     it("Should handle non successful requests", async () => {
@@ -47,5 +76,17 @@ describe("Location Service", () => {
         fetch.mockResponseOnce("{}", { status: 500, headers: { "content-type": "application/json" } });
 
         expect(await searchCities("this value does not matter")).toEqual([]);
+    });
+
+    it("Should handle aborted requests or requests with network errors", async () => {
+
+        // Simulate request error
+        fetch.mockReject(new Error("fake error message"));
+
+        try {
+            await searchCities("this value does not matter");
+        } catch (err) {
+            expect(err).toEqual([{ msg: Constants.UNEXPECTED_ERROR_MESSAGE }]);
+        }
     });
 });
