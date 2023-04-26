e5ea264dbeb97657e92be95fdd585019a17e3a53
processing <rbruno.p.o@gmail.com>
2023-04-26 14:55:22 +0100

    Changed chip colors.

--- src/components/Company/Offers/Manage/CompanyOffersTitle.js
+++ src/components/Company/Offers/Manage/CompanyOffersTitle.js
@@ -5,8 +5,8 @@ import { Chip } from "@material-ui/core";
 export const statusChips = {
     active: <Chip label="Active" size="small" style={{ backgroundColor: "#4CAF50" }} />,
     hidden: <Chip label="Hidden" size="small" style={{ backgroundColor: "#90A4AE" }} />,
-    blocked: <Chip label="Blocked" size="small" style={{ backgroundColor: "#F44336" }} />,
-    archived: <Chip label="Archived" size="small" style={{ backgroundColor: "#2196F3" }} />,
+    blocked: <Chip label="Blocked" size="small" style={{ backgroundColor: "#DC4338" }} />,
+    archived: <Chip label="Archived" size="small" style={{ backgroundColor: "#56A8D6" }} />,
 };
 
 const OfferTitle = ({ title, offerVisibilityState }) => {
